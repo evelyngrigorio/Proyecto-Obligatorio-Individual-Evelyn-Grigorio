0478604957b6382e20e582cf68dea50e669f355d
evelyngrigorio <evelyngrigorioaltier@gmail.com>
2022-08-20 12:54:46 -0300

    Modificación del loggin
    Se modifica dando estilo al formulario de inicio.
    Con funcionalidad de visualizar contraseña.
    Se agrega imagen del loggin.

---
+++ js/login.js
@@ -0,0 +1,11 @@
+function mostrarPassword(){
+    var cambio = document.getElementById("typePasswordX-2");
+    if(cambio.type == "password"){
+        cambio.type = "text";
+        $('.icon').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
+    }else{
+        cambio.type = "password";
+        $('.icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
+    }
+} 
+
