dcefb9de8cf0fa475e213e0fb879512294387ae8
evelyngrigorio <evelyngrigorioaltier@gmail.com>
2022-08-21 19:28:45 -0300

    corrección estilos

--- js/products.js
+++ js/products.js
@@ -2,13 +2,13 @@ const url = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 
 function htmlContentToAppend(products) {
     return `
-    <div class="list-group item list-group-item-action">
-        <div class ="row>
-            <div class="col-3">
-            <img src="${products.image}" alt="${products.description}" class="img-thumbnail">
-            </div>
+    <div class="list-group-item list-group-item-action">
+        <div class ="row">
             <div class="col">
-                <div class="d-flex w-100 justify-content-between">
+                <div class="col-3">
+                 <img src="${products.image}" alt="${products.description}" class="img-thumbnail">
+                 </div>
+                 <div class="d-flex justify-content-between">
                     <div class="mb-1">
                     <h4>`+ products.name +` - `+ products.currency +` `+ products.cost +`</h4>
                     <p> `+ products.description +` </p>
