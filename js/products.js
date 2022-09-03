cc0bd0f67ab2ccc46b30bc06de72947ea7bdf09e
evelyngrigorio <evelyngrigorioaltier@gmail.com>
2022-09-03 04:13:55 -0300

    categorias dinamicas
    Agregando la funcionalidad para mostrar los productos de las diferentes categorías al hacer click en ellas.

--- js/products.js
+++ js/products.js
@@ -1,19 +1,26 @@
-const url = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+const url = `https://japceibal.github.io/emercado-api/cats_products/${localStorage.getItem("catID")}.json`;
 
 function htmlContentToAppend(products) {
     return `
-    <div class="list-group-item list-group-item-action">
-        <div class ="row">
-            <div class="col">
-                <div class="col-3">
-                 <img src="${products.image}" alt="${products.description}" class="img-thumbnail">
-                 </div>
-                 <div class="d-flex justify-content-between">
-                    <div class="mb-1">
-                    <h4>`+ products.name +` - `+ products.currency +` `+ products.cost +`</h4>
-                    <p> `+ products.description +` </p>
+    <div class="row">
+        <div class="text-center p-4">
+            <p class="lead">Verás aquí todos los productos de la categoría: ${localStorage.getItem("catName")} .</p>
+        </div>
+        <div class="list group">
+            <div class="list-group-item list-group-item-action">
+                <div class ="row">
+                    <div class="col">
+                        <div class="col-3">
+                        <img src="${products.image}" alt="${products.description}" class="img-thumbnail">
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <div class="mb-1">
+                            <h4>`+ products.name +` - `+ products.currency +` `+ products.cost +`</h4>
+                            <p> `+ products.description +` </p>
+                            </div>
+                            <small class="text-muted"> ` + products.soldCount +` vendidos.</small>
+                        </div>
                     </div>
-                    <small class="text-muted"> ` + products.soldCount +` vendidos.</small>
                 </div>
             </div>
         </div>
