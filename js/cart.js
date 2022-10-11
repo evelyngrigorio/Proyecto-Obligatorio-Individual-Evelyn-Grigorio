41e84b382bae99d5384637deccfff3b272044258
evelyngrigorio <evelyngrigorioaltier@gmail.com>
2022-10-10 22:25:47 -0300

    cart v.2

--- js/cart.js
+++ js/cart.js
@@ -15,7 +15,7 @@ function showCart(){
             <td> ${cartProduct.name}</td>
             <td id="unitCost${i}">${cartProduct.currency} ${cartProduct.unitCost}</td>
             <td>
-            <input type="number" id="${i}" onchange="updateSubtotal(${i});" value="${cartProduct.count}" min="1">
+            <input type="number" id="${i}" onchange="subtotal(${i});" value="${cartProduct.count}" min="1">
             </td>
             <td id="subtotal${i}" class="fw-bold">${cartProduct.currency} ${cartProduct.count * cartProduct.unitCost}</td>
        </tr>`
@@ -24,6 +24,14 @@ function showCart(){
     }
     document.getElementById("cart-container").innerHTML = htmlContentToAppend;
 };
+
+function subtotal(i){
+    let cost = cartArray[i - 1].unitCost;
+    let count = document.getElementById(i).value;
+
+     /*Al id=subtotal+i le pasamos el resultado de la multiplicación.*/
+     document.getElementById("subtotal" + i).innerHTML = count * cost;
+}
 
 document.addEventListener("DOMContentLoaded", async function() {
     getJSONData(CART_INFO_URL).then(function (response) {
